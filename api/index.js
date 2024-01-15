2eca273bea9488f924e063737555deb63d5151c6
professor <praveencan111@gmail.com>
2024-01-15 19:42:17 +0530

    database connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import mongoose  from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app=express();
 
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("database succesfully connected")
+})
+.catch((error)=>{
+    console.log("error datebase connection")
+})
 app.listen(4000,()=>{
     console.log("server started on 4000")
 })
