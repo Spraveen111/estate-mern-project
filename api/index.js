5a3d815000bfed0b86bf12a0ca15cfeddb2e3c55
professor <praveencan111@gmail.com>
2024-01-15 21:42:32 +0530

    sign up api created with post method

--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRouter.route.js"; // Import the correct router
+import authRouter from "./routes/auth.route..js"
 
 dotenv.config();
 const app = express();
-
+app.use(express.json())
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -18,6 +19,9 @@ mongoose
 
 app.use("/api/user", userRouter); // Mount the router on /api/user path
 
+app.use("/api/auth",authRouter)
+
 app.listen(8000, () => {
   console.log("Server started on port 8000");
 });
+
